ebcfdfa56ad6e7ae8f5379f0941587181af68837
NunoFerreira95 <146824491+NunoFerreira95@users.noreply.github.com>
2023-10-03 11:45:01 +0100

    Update Trabalho_BMW_Botoes.py

--- Trabalho_BMW_Botoes.py
+++ Trabalho_BMW_Botoes.py
@@ -4,10 +4,10 @@ janela = Tk()
 janela.title("Click search button")
 janela.geometry('250x250')
 
-botao = Button(janela, text="Shows reservations list")
+botao = Button(janela, text="Shows empty list")
 botao.grid(column=0, row=0)
 
-botao2 = Button(janela, text="Shows empty list")
+botao2 = Button(janela, text="Shows reservations list")
 botao2.grid(column=1, row=0)
 
 janela.mainloop()
