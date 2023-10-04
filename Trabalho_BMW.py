cc08b3170f20149f5e6fb5fecb374f19bedeb66b
NunoFerreira95 <146824491+NunoFerreira95@users.noreply.github.com>
2023-10-04 14:13:03 +0100

    Update Trabalho_BMW.py

--- Trabalho_BMW.py
+++ Trabalho_BMW.py
@@ -25,6 +25,25 @@ while quantidade_dias>4:
 
 print("A reserva foi efetuada")
 
+h1 =input('Indique a hora inicial da reserva no formato hh:mm')
+h2=input('Indique a hora final da reserva no formato hh:mm')
+# Hora final
+hf = datetime.strptime(h2, '%H:%M')
+
+# Hora inicial
+hi = datetime.strptime(h1, '%H:%M')
+
+if quantidade_dias==0:
+    while numero_horas <1:
+        print("Reseva anulada. O número mínimo de horas é 1 e a sua reserva está com", numero_horas, "horas")
+        h1 =input('Indique a hora inicial da reserva no formato hh:mm')
+        h2=input('Indique a hora final da reserva no formato hh:mm')
+        numero_horas = (datetime.strptime(h2, '%H:%M') - datetime.strptime(h1, '%H:%M')).seconds / 3600
+    print("A reserva foi efetuada e durou ", numero_horas)
+    
+# Quando não são 0 dias e está fora do ciclo while, ou seja, quando a reserva não tem mais de 4 dias
+print("A reserva foi efetuada e durou", quantidade_dias, "dias e ", numero_horas, "horas")
+
 from tkinter import*
 
 janela = Tk()
@@ -48,20 +67,4 @@ else:
     print('Stock cheio. Escolha outra marca')
     m=input('Selecione uma marca')
 
-h1 =input('Indique a hora inicial da reserva no formato hh:mm')
-h2=input('Indique a hora final da reserva no formato hh:mm')
-# Hora final
-hf = datetime.strptime(h2, '%H:%M')
-
-# Hora inicial
-hi = datetime.strptime(h1, '%H:%M')
-
-numero_horas=h_f-h_i
-print("O número de horas durante o aluguer do veículo é ", numero_horas)
-
-              
-
-
-
-
-
+       
